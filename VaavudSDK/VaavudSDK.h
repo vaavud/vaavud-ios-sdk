1bf659fe7c41e3f8ebbeea6186b7f489221bad3c
Andreas Okholm <aokholm@gmail.com>
2015-09-13 18:30:27 +0200

    Initial Commit

---
+++ VaavudSDK/VaavudSDK.h
@@ -0,0 +1,19 @@
+//
+//  VaavudSDK.h
+//  VaavudSDK
+//
+//  Created by Andreas Okholm on 13/09/15.
+//  Copyright (c) 2015 Vaavud ApS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VaavudSDK.
+FOUNDATION_EXPORT double VaavudSDKVersionNumber;
+
+//! Project version string for VaavudSDK.
+FOUNDATION_EXPORT const unsigned char VaavudSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VaavudSDK/PublicHeader.h>
+
+
