db1890738210a9453abd30d77d67efe05dd5eb34
GreatApe <kugel@kth.se>
2016-01-14 15:40:10 +0100

    even cleaner

--- VaavudSDK/Vaavud.swift
+++ VaavudSDK/Vaavud.swift
@@ -23,7 +23,7 @@ public class VaavudSDK: WindListener, LocationListener {
     public private(set) var session = VaavudSession()
     
     public var windSpeedCallback: (WindSpeedEvent -> Void)?
-    public var trueWindSpeedCallback: (WindSpeedEvent -> Void)?
+    public var trueWindSpeedCallback: (WindSpeedEvent -> Void)? // fixme: implement
     public var windDirectionCallback: (WindDirectionEvent -> Void)?
     public var trueWindDirectionCallback: (WindDirectionEvent -> Void)? // fixme: implement
     public var temperatureCallback: (TemperatureEvent -> Void)? // fixme: implement
